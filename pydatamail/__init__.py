70d942356ff3c4238244a89f5e851c4e05664e82
Jan Janssen <jan.janssen@outlook.com>
2022-02-13 14:14:56 -0700

    black formating

--- pydatamail/__init__.py
+++ pydatamail/__init__.py
@@ -1,3 +1,7 @@
 from pydatamail.database import DatabaseInterface
-from pydatamail.plots import get_from_pie_plot, get_labels_pie_plot, get_number_of_email_plot
+from pydatamail.plots import (
+    get_from_pie_plot,
+    get_labels_pie_plot,
+    get_number_of_email_plot,
+)
 from pydatamail.message import Message, email_date_converter
